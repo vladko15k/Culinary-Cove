39eb350b9c70c147db2fe3dee1357689553c8a6a
vladko15k <vlatko15k@gmail.com>
2025-06-05 12:02:15 +0300

    add include folder for files

--- ingredients.php
+++ ingredients.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./styles/simple.css" />
-    <link rel="stylesheet" href="./styles/custom.css" />
-    <title>Culinary Cove &bull; Our mission</title>
-  </head>
-  <body>
-    <header
-      class="header-with-background"
-      style="background-image: url('images/pexels-engin-akyurt-1435904.jpg')"
-    >
-      <h1>Culinary Cove</h1>
-      <p>Your sanctuary for exceptional flavors</p>
-      <nav>
-        <a href="our-mission.php">Our mission</a>
-        <a href="ingredients.php">Ingredients</a>
-      </nav>
-    </header>
-
-    <main>
+<?php include 'inc/header.inc.php'; ?>
       <h1>Our ingredients</h1>
       <p>
         At Culinary Cove, our ethos is deeply rooted in the belief that
@@ -50,10 +28,4 @@
         ingredients that transforms dining at Culinary Cove into more than just
         a meal—it becomes a journey into a world of pure, unadulterated taste.
       </p>
-    </main>
-
-    <footer>
-      <p>Culinary Cove's website.</p>
-    </footer>
-  </body>
-</html>
+    <?php include 'inc/footer.inc.php'; ?>
